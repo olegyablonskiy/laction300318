33e501b5d06b41c2430db0a58133e94003f9d053
oleg yablonskiy <olegyablonskiy1993@gmail.com>
2018-03-30 16:54:03 +0000

    in

---
+++ index.js
@@ -0,0 +1,20 @@
+// for (let i = 0; i < 100; i++) {
+//     if (i % 2) {
+//         continue;
+//     }
+//     prn(i);
+// }
+
+let i = 1;
+while ( i <= 100;) {
+    prn(i);
+    i++;
+}
+
+do {
+    
+} while ()
+
+function prn(val) {
+    document.write(val + '<br>');
+}
